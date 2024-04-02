d1662329e3d4b2b19562ef75b4da25060a8a7067
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-04-02 00:09:13 -0500

    "Exercise Data Types is Resubmitted!"

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/Search.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/Search.java
@@ -1,7 +1,29 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class Search {
     public static void main(String[] args) {
-        String text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’";
+        String text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of " +
+                "having nothing to do: once or twice she had peeped into the book her sister " +
+                "was reading, but it had no pictures or conversations in it, ‘and what is the use " +
+                "of a book,’ thought Alice ‘without pictures or conversation?’";
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter word to search");
+        String inputWord = input.next();
+
+        if( text.toLowerCase().contains(inputWord.toLowerCase())){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        int index = text.toLowerCase().indexOf(inputWord.toLowerCase());
+        int length = inputWord.length();
+        System.out.println("Word " + inputWord + " of length " + length + " is found at index " + index);
+        String updatedText = text.toLowerCase().replace(inputWord.toLowerCase(), "");
+        System.out.println(updatedText);
+
     }
 }
