a4efc5729d2d9dff4b0fc82f59aae4733cfd01db
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-04-04 14:02:35 -0500

    "collection exercises Done"

---
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayPractice.java
@@ -0,0 +1,20 @@
+package org.launchcode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class ArrayPractice {
+    public static void main (String[] args){
+      int[] array = {1, 1, 2, 3, 5, 8};
+     for (double newArray : array){
+         if(newArray%2 !=0) {
+             System.out.println(newArray);
+         }
+     };
+     String sentence ="I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+     String[] sentenceArray = sentence.split("\\. ");
+     System.out.println(Arrays.toString(sentenceArray));
+    }
+}
