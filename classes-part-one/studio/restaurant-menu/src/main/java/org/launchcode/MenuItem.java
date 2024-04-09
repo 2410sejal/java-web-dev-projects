661248d1f4f47f508f7ce5c706fc0c000a281a1f
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-04-08 20:53:20 -0500

    "Classes Part1 Studio Done"

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import com.sun.tools.javac.jvm.Items;
+
+import java.util.ArrayList;
+
+public class MenuItem {
+ private ArrayList<Items> brunchItems;
+    private ArrayList<Items> appetizer;
+    private ArrayList<Items> mainCourse;
+    private ArrayList<Items> dessert;
+}
