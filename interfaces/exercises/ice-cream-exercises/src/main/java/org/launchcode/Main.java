8cd5d4d778d1611923b84856a31d9bb3d14b9695
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-04-22 17:20:38 -0500

    "Interface excersise Done"

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,5 +1,5 @@
 package org.launchcode;
-
+import java.util.Comparator;
 import java.util.ArrayList;
 
 public class Main {
@@ -9,9 +9,13 @@ public class Main {
         ArrayList<Cone> cones = menu.getCones();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+        Comparator comparator = new FlavorComparator();
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+        flavors.sort(comparator);
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println(flavors);
+        System.out.println(cones);
     }
 }
